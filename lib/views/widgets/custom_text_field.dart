1a920ac26c0bc8f95b7444c551b8362e5d4e9b0f
mahmoud khaled <you@example.com>
2023-01-13 16:49:25 +0200

    take_inpute&valadite_data

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,16 +1,30 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:note_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextField({
+    super.key,
+    required this.hint,
+    this.maxLines = 1,
+    this.onsave,
+    this.valadate,
+  });
 
   final String hint;
   final int maxLines;
+  final void Function(String?)? onsave;
+  final String? Function(String?)? valadate;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onsave,
+      validator: (value) {
+        if (value?.isEmpty ?? true) return "must enter value";
+      },
       cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
